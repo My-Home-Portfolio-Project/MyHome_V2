cf8fc0c26fccf2678c3e6dd2049ea5c1e65c5c23
Mbaruku2003 <leonardjembe21@gmail.com>
2025-01-02 16:53:52 +0300

    modified registration page to accept username, email, name and password

--- myHome/templates/auth/process-signup.php
+++ myHome/templates/auth/process-signup.php
@@ -1,24 +1,45 @@
+
 <?php
 if (empty($_POST["name"])) {
     die("Name is required");
 }
-if (! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
     die("Invalid email format");
 }
 if (strlen($_POST["password"]) < 9) {
     die("Password must be at least 9 characters long");
 }
-if ( ! preg_match("/[0-9]/", $_POST["password"]) ) {
+if (!preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
-if ( ! preg_match("/[A-Z]/", $_POST["password"]) ) {
+if (!preg_match("/[A-Z]/", $_POST["password"])) {
     die("Password must contain at least one uppercase letter");
 }
-if ( ! preg_match("/[a-z]/", $_POST["password"]) ) {
+if (!preg_match("/[a-z]/", $_POST["password"])) {
     die("Password must contain at least one lowercase letter");
 }
 if ($_POST["password"] !== $_POST["confirm_password"]) {
     die("Passwords do not match");
 }
-$password_hash($_POST["password"], PASSWORD_DEFAULT);
-$mysqli = require __DIR__ . "./database.php";
+
+// Hash the password
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)";
+
+$stmt = $mysqli->stmt_init();
+
+if (!$stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param(
+    "sss",
+    $_POST["name"],
+    $_POST["email"],
+    $password_hash    
+);
+$stmt->execute();
+echo "User succesfully sugned up";
